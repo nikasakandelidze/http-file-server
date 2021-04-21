4e629bdfbfb62ad492d0229b83f5aa850a253e3d
sakana <nikasakana@gmail.com>
2021-04-21 20:13:13 +0400

    updated document root for http file server

--- http_server_requester.py
+++ http_server_requester.py
@@ -101,9 +101,13 @@ def _get_document_root_for_vhost(_port, host_header_value):
 
 
 def is_host_header_valid(host_header_value):
-    for x in virtual_hosts.keys():
-        for y in virtual_hosts[x]:
-            if y[1] in host_header_value:
+    for virutal_host_key in virtual_hosts.keys():
+        for vhost_tuple in virtual_hosts[virutal_host_key]:
+            vhost_tuple_ = vhost_tuple[1]
+            header_value = './statics/' + host_header_value.strip()
+            value = vhost_tuple_ == header_value
+            tuple_ = header_value == vhost_tuple_
+            if value or tuple_:
                 return True
     return False
 
